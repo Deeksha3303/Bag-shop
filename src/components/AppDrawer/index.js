388431119fc4f033f658f838d391c46a1cf26cdf
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-06-18 18:53:45 +0530

    Update index.js

--- src/components/AppDrawer/index.js
+++ src/components/AppDrawer/index.js
@@ -1,1 +1,44 @@
+import { Drawer, List, ListItemText ,ListItemButton, Divider} from "@mui/material";
+import { ContexUi } from "../../context/contexUI";
+import { DrawerButtonClose } from "../../styles/appbar";
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import { Colors } from "../../styles/theme";
+import { lighten } from "polished";
 
+ 
+ export default function AppDrawer(){
+     
+    const {drawer,setdrawer}=ContexUi();
+
+    return (
+        <>
+         {drawer && ( <DrawerButtonClose className="testing"><CloseOutlinedIcon onClick={()=>setdrawer(false)} 
+         sx={{fontSize:"2.5rem", color:lighten(0.09,Colors.primary) }}></CloseOutlinedIcon></DrawerButtonClose>)}
+       <Drawer open={drawer}>
+        <List>
+            <ListItemButton>
+                <ListItemText> </ListItemText>
+            </ListItemButton>
+            <Divider orientation="horizontal"  variant="middle"></Divider>
+            <ListItemButton>
+                <ListItemText> </ListItemText>
+            </ListItemButton>
+            <Divider orientation="horizontal" variant="middle"></Divider>
+            <ListItemButton>
+                <ListItemText> </ListItemText>
+            </ListItemButton>
+            <Divider orientation="horizontal" variant="middle"></Divider>
+            <ListItemButton>
+                <ListItemText> </ListItemText>
+            </ListItemButton>
+            <Divider orientation="horizontal" variant="middle"></Divider>
+            
+            <ListItemButton>
+                <ListItemText>Contact us</ListItemText>
+            </ListItemButton>
+
+        </List>
+       </Drawer>
+       </>
+    )
+ }
