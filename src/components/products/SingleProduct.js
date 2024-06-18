438de0aa391e47d5481897bd57740337a84223cd
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-06-18 15:36:47 +0530

    Update SingleProduct.js

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -36,7 +36,7 @@ export default function SingleProduct ({product, matches}){
                 </ProductActionButton>
                 </Stack>
             </ProductActionsWrapper>
-            <ProductAddToCart > Add to cart</ProductAddToCart>
+            <ProductAddToCart > BOOK </ProductAddToCart>
             
         </Product>
         
