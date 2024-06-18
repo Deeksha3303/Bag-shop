ccac9746dde24fdb680898a7060edc3b22a23d92
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-06-18 21:07:57 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 1,
         name: "Veterinary Services",
-        price: 299.99,
+        price: 19.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image:"images/banner/products/bag_01.jpeg",
         
@@ -12,35 +12,35 @@ export const products = [
     {
         id: 2,
         name: "Grooming",
-        price: 239.99,
+        price: 35,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "images/banner/products/bag_02.png"
     },
     {
         id: 3,
         name: "Pet-Sitting",
-        price: 189.99,
+        price: 10,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "images/banner/products/bag_03.png"
     },
     {
         id: 4,
         name: "Pet Training",
-        price: 129.99,
+        price: 29.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "images/banner/products/bag_04.jpg"
     },
     {
         id: 5,
         name: "Nutrition and Diet Services",
-        price: 399.99,
+        price: 27.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "images/banner/products/bag_05.jpeg"
     },
     {
         id: 6,
         name: "Emergency Care",
-        price: 689.99,
+        price: 50,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
         image: "images/banner/products/bag_06.png"
     }
