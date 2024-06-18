be12aa0968208ceb3d5218a6f4f0e79013ea9896
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-06-18 19:39:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-
-import {Button,Container, Typography} from '@mui/material';
-import {useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SingleProduct from "./components/SingleProduct"; // Adjust the path as needed
+import BookPage from "./components/BookPage"; // Create this component
+import AppBar from './components/AppBar';
+import Banner from './components/Banner';
+import Promotions from './components/Promotions';
+import Products from './components/Products';
+import Footer from './components/Footer';
+import AppDrawer from './components/AppDrawer';
+import SearchBox from './components/Search';
+import { UIprovider } from './context/contextUI';
+import { Container, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
-import AppBar from './components/appbar';
-import Banner from './components/banner';
-import Promotions from './components/promotions';
-import Products from './components/products';
-import { FolderOffSharp } from '@mui/icons-material';
-import Footer from './components/foooter';
-import AppDrawer from './components/AppDrawer';
-import { UIprovider } from './context/contexUI';
-import SearchBox from './components/search';
-
+import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    document.title="E-bag shop"
-  
-   
-  }, [])
-  
-  return (
-    <ThemeProvider theme = {theme}>
- <Container maxWidth="xl" sx={{backgroundColor:"#fff"}}>
-  <UIprovider>
-      <AppBar></AppBar>
-      <Banner></Banner>
-      <Promotions></Promotions>
-      <div>
-        <Typography variant="h4" padding={"4px"} display="flex" alignItems="center" justifyContent={"center"}> Our Products</Typography>
-      </div>
-      <Products></Products>
-      <Footer></Footer>
-      <AppDrawer></AppDrawer>
-      <SearchBox></SearchBox>
-  </UIprovider>
-      
-    
-      {/*
- appbar 
-  promotions
-  tittle
-  products
-  searchbox
-  appdrawer*/}
-    
-    </Container>
+    document.title = "E-bag shop";
+  }, []);
 
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Container maxWidth="xl" sx={{ backgroundColor: "#fff" }}>
+          <UIprovider>
+            <AppBar />
+            <Banner />
+            <Promotions />
+            <div>
+              <Typography variant="h4" padding={"4px"} display="flex" alignItems="center" justifyContent={"center"}> Our Products</Typography>
+            </div>
+            <Products />
+            <Footer />
+            <AppDrawer />
+            <SearchBox />
+          </UIprovider>
+        </Container>
+        <Switch>
+          <Route path="/" exact component={HomePage} /> {/* Your home page component */}
+          <Route path="/book" component={BookPage} /> {/* The page to navigate to */}
+        </Switch>
+      </Router>
     </ThemeProvider>
-   
- 
   );
 }
 
