59a24cfe7563a915ddf5eb776037e2cbc17e451d
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-06-18 19:41:03 +0530

    Create Bookpage.js

---
+++ src/components/products/Bookpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookPage = () => {
+  return (
+    <div>
+      <h1>Book Page</h1>
+      <p>This is the page you navigate to when the "BOOK" button is clicked.</p>
+    </div>
+  );
+}
+
+export default BookPage;
