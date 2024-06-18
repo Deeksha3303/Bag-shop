0e9aa788a65f774353b71d36afe629d3891b9800
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-06-18 21:19:45 +0530

    Update SingleProduct.js

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -1,4 +1,7 @@
 
+import FavoriteBorderOutlined from "@mui/icons-material/FavoriteBorderOutlined";
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import FitScreenOutlinedIcon from '@mui/icons-material/FitScreenOutlined';
 import { Stack } from "@mui/material"
 import {    ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage, Productmetawrapper } from "../../styles/product"
 import ProductMeta from "./ProductMeta"
@@ -13,24 +16,29 @@ export default function SingleProduct ({product, matches}){
 
     return (
     <>
-   <Product>
-        <ProductImage src={product.image}></ProductImage>
-        <ProductMeta product={product} matches={matches}></ProductMeta>
-        <ProductActionsWrapper>
-          <Stack direction="row">
-            <ProductFavButton isfav={1}>
-              <FavoriteBorderOutlined></FavoriteBorderOutlined>
-            </ProductFavButton>
-            <ProductActionButton>
-              <ShareOutlinedIcon></ShareOutlinedIcon>
-            </ProductActionButton>
-            <ProductActionButton>
-              <FitScreenOutlinedIcon></FitScreenOutlinedIcon>
-            </ProductActionButton>
-          </Stack>
-        </ProductActionsWrapper>
-        <ProductAddToCart onClick={handleBookClick}>BOOK</ProductAddToCart>
-      </Product>
+      
+    <Product >
+            <ProductImage src={product.image}></ProductImage>
+           
+            
+            <ProductMeta product={product} matches={matches}></ProductMeta>
+           
+            <ProductActionsWrapper >
+                <Stack direction="row">
+                <ProductFavButton isfav={1}>
+                    <FavoriteBorderOutlined ></FavoriteBorderOutlined> 
+                </ProductFavButton>
+                <ProductActionButton>
+                <ShareOutlinedIcon></ShareOutlinedIcon>
+                </ProductActionButton>
+                <ProductActionButton>
+                    <FitScreenOutlinedIcon></FitScreenOutlinedIcon>
+                </ProductActionButton>
+                </Stack>
+            </ProductActionsWrapper>
+            <ProductAddToCart > BOOK </ProductAddToCart>
+            
+        </Product>
         
         </>
         
